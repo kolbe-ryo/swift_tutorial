738550f788e2bb5d598454c5890f3570371fea94
kolbe <water.northground@gmail.com>
2023-11-03 01:14:07 +0900

    cool animated transition

--- Tutorial/View/ContentView.swift
+++ Tutorial/View/ContentView.swift
@@ -2,31 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            CircleImage()
-            VStack(alignment: .leading) {
-                Text("Hello, Swift")
-                    .font(.title2)
-                    .bold()
-                    .foregroundColor(.blue)
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                Divider()
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            .background(Color.white)
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
