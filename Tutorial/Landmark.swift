6134dd98d1b7aadd9c1195e89ff083055100f391
kolbe <water.northground@gmail.com>
2023-11-03 00:10:38 +0900

    add image

--- Tutorial/Landmark.swift
+++ Tutorial/Landmark.swift
@@ -1,9 +1,16 @@
 import Foundation
+import SwiftUI
 
 struct Landmark: Hashable, Codable {
-    var id: Int
-    var name: String
-    var park: String
-    var state: String
-    var description: String
+    private var id: Int
+    private var name: String
+    private var park: String
+    private var state: String
+    private var description: String
+    
+    private var imageName: String
+    
+    var image: Image {
+        Image(imageName)
+    }
 }
