252cfc78a0334cd244ef51de0ab9b78bd84e0c62
kolbe <water.northground@gmail.com>
2023-11-03 00:37:17 +0900

    new file

---
+++ Tutorial/View/LnadmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LnadmarkRow.swift
+//  Tutorial
+//
+//  Created by Ryo Fukahori on 2023/11/03.
+//
+
+import SwiftUI
+
+struct LnadmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LnadmarkRow()
+}
