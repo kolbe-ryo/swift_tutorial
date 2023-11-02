2c5a4d4eb0f7d076993368c0444b0d56523e17dd
kolbe <water.northground@gmail.com>
2023-11-03 00:47:55 +0900

    create LandmarkList

---
+++ Tutorial/View/LandmarkList.swift
@@ -0,0 +1,15 @@
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List{
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
