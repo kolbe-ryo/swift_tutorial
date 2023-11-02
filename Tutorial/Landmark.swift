609f57be47ce5e3e34c117a349c5d7f6cde12ed5
kolbe <water.northground@gmail.com>
2023-11-03 00:16:15 +0900

    change struct declaration

--- Tutorial/Landmark.swift
+++ Tutorial/Landmark.swift
@@ -17,14 +17,14 @@ struct Landmark: Hashable, Codable {
         Image(imageName)
     }
     
-    struct Coordinates: Hashable, Codable {
-        var latitude: Double
-        var longitude: Double
-    }
-    
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
             longitude: coordinates.longitude)
     }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
 }
