b75be02d35c428efd6f1ae6c24aafb1af226fd7a
kolbe <water.northground@gmail.com>
2023-11-10 21:56:38 +0900

    step 4

---
+++ Tutorial/View/PageView/PageViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import SwiftUI
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])],
+            direction: .forward, 
+            animated: true
+        )
+    }
+    
+}
