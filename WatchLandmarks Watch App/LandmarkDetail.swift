ba5bdea2a828d2dc7ed70edbf506368226e21a0c
kolbe <water.northground@gmail.com>
2023-11-13 21:02:02 +0900

    section3

--- WatchLandmarks Watch App/LandmarkDetail.swift
+++ WatchLandmarks Watch App/LandmarkDetail.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     @Environment(ModelData.self) var modelData
+    
     var landmark: Landmark
     
     var landmarkIndex: Int {
@@ -12,8 +13,44 @@ struct LandmarkDetail: View {
     }
     
     var body: some View {
-        CircleImage(image: landmark.image.resizable())
-            .scaledToFill()
+        @Bindable var modelData = modelData
+        
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+                
+                
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+                
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+                
+                
+                Divider()
+                
+                
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                
+                Text(landmark.state)
+                    .font(.caption)
+                
+                Divider()
+                
+                MapView(coordinate: landmark.locationCoordinate)
+                    .scaledToFit()
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
     }
 }
 
@@ -21,6 +58,6 @@ struct LandmarkDetail: View {
     let modelData = ModelData()
     
     return LandmarkDetail(
-        landmark: modelData.landmarks[1]
+        landmark: modelData.landmarks[0]
     ).environment(modelData)
 }
