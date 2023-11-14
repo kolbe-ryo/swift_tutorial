ec2a11985a71832c06d59d2c682c880ffcbe6f0c
kolbe <water.northground@gmail.com>
2023-11-14 23:24:19 +0900

    section5-2

--- WatchLandmarks Watch App/NotificationView.swift
+++ WatchLandmarks Watch App/NotificationView.swift
@@ -2,8 +2,25 @@
 import SwiftUI
 
 struct NotificationView: View {
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let landmark {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+            }
+            
+            Text(title ?? "Unknown Landmark")
+                .font(.headline)
+            
+            Divider()
+            
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
+                .font(.caption)
+        }
     }
 }
 
